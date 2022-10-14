52a5f09b142d288406072e3b8fb2d9ecf2607239
wesleywil <wesleywilsonti@gmail.com>
2022-10-14 14:03:11 -0300

    added image to homepage and some made homepage more responsive

--- src/pages/homepage.svelte
+++ src/pages/homepage.svelte
@@ -4,10 +4,15 @@
 
 <div class="flex  items-center screen_size">
   <div
-    class="text-3xl text-left lg-color flex flex-col gap-2 p-4 pl-8 xl:w-1/2 md:w-2/3"
+    class="text-2xl xl:text-3xl md:text-3xl text-left lg-color flex flex-col gap-2 p-4 pl-8 xl:w-1/2 md:w-2/3"
   >
-    <h1 class="text-8xl font-bold">
-      <span class="md-color">X</span>-Music
+    <h1 class="text-6xl xl:text-8xl md:text-8xl font-bold flex items-center">
+      <img
+        src="public/X-music.svg"
+        alt="logo"
+        class="w-16 xl:w-24 md:w-24 mt-4 rotate-90 self-center"
+      />
+      Music
     </h1>
     <p>X-music is the ultimate destination for music lovers of all kinds!</p>
     <p>
@@ -22,10 +27,14 @@
       >
     </div>
   </div>
+  <div class="w-1/3 p-2 ml-12 hidden xl:block md:block">
+    <img src="public/homepage_img.svg" alt="music_player" />
+  </div>
 </div>
 
 <style>
   .screen_size {
-    height: 92vh;
+    min-height: 92vh;
+    max-width: 100%;
   }
 </style>
